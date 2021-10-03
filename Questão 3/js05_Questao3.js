f0a92990d028f6af087738b3761c166468428f20
João Paulo de Oliveira <88550957+jpoliveiradev@users.noreply.github.com>
2021-10-02 23:27:01 -0300

    Update js05_Questao3.js

--- Questão 3/js05_Questao3.js
+++ Questão 3/js05_Questao3.js
@@ -1,26 +1,21 @@
-var js_cidadeEstado;
-var js_nCriancasNasc;
-var js_nObitos;
-var js_nHabitantes;
-var js_taxaNatalidade;
-var js_taxaMortalidade;
+var js_cidadeEstado,  js_nCriancasNasc , js_nObitos, js_nHabitantes, js_taxaNatalidade, js_taxaMortalidade;
 var js_opc;
 
 function calcularTaxa() {
-    js_cidadeEstado = document.taxaNeM.fm_city.value;
-    js_nCriancasNasc = parseInt(document.taxaNeM.fm_nCriancasNasc.value);
-    js_nObitos = parseInt(document.taxaNeM.fm_nObitos.value);
-    js_nHabitantes = parseInt(document.taxaNeM.fm_nHabitantes.value);
+    js_cidadeEstado = document.formTaxa.lugar.value;
+    js_nCriancasNasc = Number(document.formTaxa.criNascidas.value);
+    js_nObitos = Number(document.formTaxa.obitos.value);
+    js_nHabitantes = Number(document.formTaxa.habitantes.value);
 
-    js_opc = parseInt(window.prompt("Escolha uma das opções:" + "\n1- Taxa de Natalidade;" +"\n2- Taxa de Mortalidade."));
+    js_opc = Number(window.prompt("Escolha uma das opções:" + "\n1- Taxa de Natalidade;" +"\n2- Taxa de Mortalidade."));
 
     switch (js_opc){
         case 1: js_taxaNatalidade = ((js_nCriancasNasc * 1000) / js_nHabitantes);
-            document.write("<h2>" + "Cidade/estado: " +js_cidadeEstado+ "</h2>");
-            document.write("<h2>" + "Taxa de Natalidade: " +js_taxaNatalidade+ "</h2>"); break;
+        document.write(`<h2> Cidade/estado:  ${js_cidadeEstado} </h2>`);
+        document.write(`<h2> Taxa de Natalidade:  ${js_taxaNatalidade} </h2>`); break;
         case 2: js_taxaMortalidade = ((js_nObitos * 1000) / js_nHabitantes);
-            document.write("<h2>" + "Cidade/estado: " +js_cidadeEstado+ "</h2>");
-            document.write("<h2>" + "Taxa de Mortalidade: " +js_taxaMortalidade+ "</h2>"); break;
+            document.write(`<h2> Cidade/estado:  ${js_cidadeEstado} </h2>`);
+            document.write(`<h2> Taxa de Mortalidade:  ${js_taxaMortalidade} </h2>`); break;
         default : document.write("Tente Novamete!"); break;
     }
 }
